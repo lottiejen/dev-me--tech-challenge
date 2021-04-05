472b2f91969eb9b7408875a75ba59cd2f5794adb
Lottie <lottiej17@gmail.com>
2021-04-05 15:41:13 +0100

    Added / edited makeButton, which is working adds teams using interpolation + concat and mathrandom

--- javascript/app.js
+++ javascript/app.js
@@ -14,7 +14,7 @@
     let plusButton = document.getElementById("plusbutton") // adding player
     let input = document.getElementById("player") // input field for player to be added 
     let ul = document.getElementById("list") // list where 'input' AKA players will display 
-    let makeButton = document.getElementById("makebutton") // button for making a team :) 
+    let makeButton = document.getElementById("makebutton") // button for making a team
     let team = document.getElementById("random-list") // this is the final list where teams are displayed 
     
     // editing variables --- as not specfic enough below
@@ -31,16 +31,15 @@
     const list = []; 
 
     plusButton.addEventListener ("click", () => {
+      
 
         if (input.value.length == 0) {   // setting to stop user = entering an empty string -- keep this?? 
             alert("Please enter a player name");
         }  
 
-        let li = document.createElement("li"); // Adds input to list 
+        let li = document.createElement("li"); // creating a new list item when you click 'Add' 
         li.textContent = input.value; // what user has typed
-
-        // console.log(li.textContent);
-        li.classList.add("list-group");// plus?
+        li.classList.add("list-group"); // add to list
         ul.prepend(li);
         
         list.push (input.value)
@@ -56,24 +55,33 @@
 //    // https://www.w3schools.com/jsref/jsref_sort.asp <-- for ref
 //    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random <-- need this also somewhere 
 
-//     makebutton.addEventListener ("click", () => { // adding EL 
+    // MAKE TEAM 
 
-//        let names = list;  // names = list
-//        names.sort(() => 0.5 - Math.random()); // randomize names
-//        let bracket = []; // put pairs go into empty array
+    makeButton.addEventListener ("click", () => { // adding EL 
 
-//        // - can we use 'while' loop here? - poss if/else?
-//        while (names.length >= 2) {
-//            let bracket = `${names.pop}()} ${"VS."} ${names.pop()}`
+        // variables 
+
+       let names = list;  // names = list
+       let partners = []; // feed into empty array
+
+       names.sort(() => 0.5 - Math.random()); // randomize names
+
+       // - tried if/else. Use 'while' maybe? 
+       while (names.length >= 2) {
+           let partner = `${names.pop()} ${"VS."} ${names.pop()}` // interpolate + concat?
           
-//            bracket.push(bracket); // pushes bracker/pair created 
+           // this *should* push the partners who've been created 
 
-//            let lastlist = docment.createElement('p')
-//            lastlist.text = bracket; 
-//            lastlist.classList.add("list-group")
-//            ul.prepend(lastlist)
-//        }
+          partners.push(partner); // pushes partners cretaed 
+           console.log(partner);
 
-//     });
+           let lastlist = document.createElement('p')
+           lastlist.textContent = partner; 
+           lastlist.classList.add("list-group")
+        //    ul.prepend(lastlist)
+           team.prepend(lastlist);
+       }
+
+    });
    
 })(document);
