9aa9e7ff5c3e657d08def35cb0a2513941df6109
Lottie <lottiej17@gmail.com>
2021-04-10 17:22:00 +0100

    Udated the mathrandom() to use the Fisher-Yates shuffle, tidied up notes too

--- javascript/app.js
+++ javascript/app.js
@@ -1,119 +1,93 @@
-// The core of the challenge is to create a tool which randomly creates pairings for a table tennis tournament bracket from a list of names collected from the user.
-
 ((document) => {
 
-    // console.log(document);
-    console.log("Hello!");
 
     // setting variables 
+    let input = document.getElementById("player") // input field for player to be added
     let plusButton = document.getElementById("plusbutton") // adding player
-    let input = document.getElementById("player") // input field for player to be added 
     let ul = document.getElementById("list") // list where 'input' AKA players will display 
     let makeButton = document.getElementById("makebutton") // button for making a team
-    let team = document.getElementById("random-list") // this is the final list where teams are displayed 
-    // const PowerOfTwo = (x) => {
-    //     return x >= 4 && Math.log2(x) % 1 === 0;
-    // }
-    
-  
-    // ADDING PLAYER NAME 
+    let pairs = document.getElementById("random-list") // this is the final list 
+    let list = [];  // setting empty array for adding players 
 
-    const list = []; 
+    // ADDING PLAYER NAME
+
+    console.log("Hello!")
 
     plusButton.addEventListener ("click", () => {
       
         let name = input.value; // value user has entered 
 
-
         // if user enters nothing in input then alert pops up
         if (name.length == "" ) {   // setting to stop user entering an empty string 
-            alert("please enter a player name")
+            alert("Error: Please enter a player name")
 
         }  
 
         else {
             
-            let li = document.createElement("li"); // creating new list item when you click 'Add' 
-            li.textContent = input.value; // what user has typed
+            let li = document.createElement("li"); // creating new list item 
+            li.textContent = input.value; // what user has typed (AKA NAME)
             li.classList.add("list-group"); // add to list
-            // ul.prepend(li);
             ul.append(li); // append      
     
-            // pushes list item created
+            // pushes the name into the end of the array 
             list.push(input.value)
+
+            // here we set input back to empty, so user doesn't need to delete prior info
             input.value = ""; 
     
             console.log(list);
     
-            // return list;
         }
-
          
     });
 
     // MAKE PAIR 
 
-    makeButton.addEventListener ("click", () => { // adding EL 
+    makeButton.addEventListener ("click", () => { 
 
         // variables 
-
-       let names = list;  // names = list
+       let players = list;  // players = 'list of players' 
        let partners = []; // feed into empty array
+       let length = input.length; 
+    
+       // Checks if the no of players is divisible by 2 & is more than 0
+       if (players.length % 2 === 0 && players.length > 0 && players.length !== 2 ) {
        
+        const arr = players; 
+        var i,
+            j,
+            temp;                               // Using Fisher-Yates shuffle for randomising  
+        for (i = arr.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }       
 
-       if (names.length % 2 === 0 && names.length > 0 && names.length !== 2 ) {
-             names.sort(() => Math.random() - Math.random());
-        
-        while (names.length >= 2) {
-            let partner = `${names.pop()} ${".VS."} ${names.pop()}` // interpolate + concat?
-            partners.push(partner); 
-        
+        //  if players more than x1 
+        while (players.length > 1) { 
 
-            // console.log(partner);
-            // partners.push(partner); 
-            let lastlist = document.createElement('p')
-            lastlist.textContent = partner; 
+            const partner = `${players.pop()} ${".VS."} ${players.pop()}` // interpolate 
+
+            partners.push(partner); // pushes the name into the end of the array 
+            
+                console.log(partner);
+ 
+            
+            let lastlist = document.createElement('li') // create li item for names to bed added to
+            lastlist.textContent = partner; // specify our partner variable
             lastlist.classList.add("list-group")
-            //    ul.prepend(lastlist)
-            //    team.prepend(lastlist);
-            team.append(lastlist);
+            pairs.append(lastlist);
         }
     }
 
-     else {
-        alert("Please enter an even number of players and a minimum of x4 players")
-     }
-
-    //    // *NEED TO SORT THIS* // 
-    //    onClick = PowerOfTwo(list.length) ? do existing logic : alert("Please enter correct amt of ppl")
-
-        //    names.sort(() => 0.5 - Math.random()); // randomize names
-        // Fisher-Yates Array randomizer 
-
-        // names.sort(() => Math.random() - Math.random()); // this seems to do  
-
-    //    names.sort(() => Math.floor(Math.random() * Math.floor(input.length)); 
+        else {
+            alert("Error: Please be sure to enter an even number and a minimum of x4 players")
+        }  
        
 
-        // this logic = names added need to be more than or equal to 4"
-    //    while (names.length >= 2) {
-    //        let partner = `${names.pop()} ${".VS."} ${names.pop()}` // interpolate + concat?
-           
-        //    console.log(names.length);
-
-       // pushes partners created
-        // partners.push(partner); 
-            
-        // console.log(partner);
-
-        //    let lastlist = document.createElement('p')
-        //    lastlist.textContent = partner; 
-        //    lastlist.classList.add("list-group")
-        //    ul.prepend(lastlist)
-        //    team.prepend(lastlist);
-            // team.append(lastlist);
-       
-
+    
     });
    
 })(document);
